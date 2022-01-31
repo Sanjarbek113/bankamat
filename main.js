c5370093d6528a9b86a31fcf63e3fe907e9370f7
Sanjarbek113 <matyoqubovsanjarbek81@gmail.com>
2022-01-31 09:44:33 +0500

    add

--- main.js
+++ main.js
@@ -4,18 +4,26 @@ const submitBtn = document.querySelector(".box-btn");
 const box = document.querySelector(".box");
 const main = document.querySelector(".main");
 const header = document.querySelector(".header");
-const mainInput = document.querySelector(".main-input");
+
 
 submitBtn.addEventListener("click", () =>{ 
     for(let key in inputelements, pulQiymati){
         pulQiymati[key].textContent = inputelements[key].value;
-    }
+    };
     box.classList.add("box-active");
    header.classList.add("header-active");
-   mainInput.classList.add("mainInput-active");
+   const mainFlex = document.querySelector(".main-flex");
+  const mainInput = document.createElement("input");
+  const mainBtn = document.createElement("button");
 
+    mainInput.classList.add("main-input");
+    mainBtn.classList.add("main-btn");
+    mainInput.textContent = "pul miqdorini kiriting";
+    mainBtn.textContent = "bosing "
+    mainFlex.appendChild(mainInput);
+    mainFlex.appendChild(mainBtn);
 
- 
+    
     
 });
 
